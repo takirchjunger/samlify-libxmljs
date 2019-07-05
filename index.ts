61fea5d1b926f1fa66c9bf72dd9d016134928fcc
takirchjunger <tom.kirchjunger@topicus.nl>
2019-07-05 17:52:34 +0200

    initial code commit

---
+++ index.ts
@@ -0,0 +1,22 @@
+import * as mod from 'libxmljs-mt';
+import * as path from 'path';
+
+const xsd = 'saml-schema-protocol-2.0.xsd';
+
+export default {
+    validate: (xml: string) => {
+        return new Promise((resolve, reject) => {
+
+            process.chdir(path.resolve(__dirname, './schemas'));
+            const xsdDoc = mod.parseXml(xsd);
+            const xmlDoc = mod.parseXml(xml);
+            const result = xmlDocValid.validate(xsdDoc)
+            if (result) {
+                return resolve('SUCCESS_VALIDATE_XML');
+            } else {
+                console.error('[ERROR] validateXML', err);
+                return reject('ERR_INVALID_XML');
+            }
+        );
+    }
+};
